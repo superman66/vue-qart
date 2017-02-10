73e287c673fcb1821e0a1fa5298b9b2601d43998
superman <supermanchc@gmail.com>
2017-02-10 16:45:31 +0800

    update demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,21 +1,7 @@
 <template>
     <div id="app">
-        <img src="./assets/logo.png">
+        <img src="logo.png">
         <h1>{{ msg }}</h1>
-        <h2>Essential Links</h2>
-        <ul>
-            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-            <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-        </ul>
-        <h2>Ecosystem</h2>
-        <ul>
-            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-            <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-        </ul>
         <div v-vue-q-art="config"></div>
     </div>
 </template>
@@ -29,10 +15,10 @@
         name: 'app',
         data () {
             return {
-                msg: 'Welcome to Your Vue.js App',
+                msg: 'Vue 2.x directive of QArt.js',
                 config: {
                     value: 'https://www.baidu.com',
-                    imagePath: './examples/assets/logo.png',
+                    imagePath: './assets/logo.png',
                     filter: 'color'
                 },
             }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 var path = require('path')
 var webpack = require('webpack')
+var merge = require('webpack-merge')
 
-module.exports = {
-  entry: './examples/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
+var base = {
+  // resolveLoader: {
+  //   root: path.join(__dirname, 'node_modules'),
+  // },
   module: {
     rules: [
       {
@@ -15,13 +13,9 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this nessessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
       },
       {
@@ -51,7 +45,7 @@ module.exports = {
     hints: false
   },
   devtool: '#eval-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
@@ -73,3 +67,28 @@ if (process.env.NODE_ENV === 'production') {
     })
   ])
 }
+
+var demo = merge(base, {
+  entry: './demo/main.js',
+  output: {
+    path: path.resolve(__dirname, './demo'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  }
+});
+
+var build = merge(base, {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'vue-qart.js',
+    library: 'VueQArt',
+    libraryTarget: 'umd'
+  }
+});
+
+module.exports = [demo, build];
+
+
+
