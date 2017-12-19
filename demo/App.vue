1d4c8302603dccdfbe4133258c02a6b2c9f96c55
Superman <supermanchc@gmail.com>
2017-12-19 16:20:34 +0800

    Add download button custom set style and text

--- demo/App.vue
+++ demo/App.vue
@@ -3,58 +3,66 @@
         <img src="logo.png">
         <h1>{{ msg }}</h1>
         <!--<div v-vue-q-art="config"></div>-->
-        <vue-q-art :config="config" :downloadButton="downloadButton"></vue-q-art>
+        <vue-q-art :config="config" ></vue-q-art>
+        <vue-q-art :config="config" :download="download"></vue-q-art>
         change qrcode url: <input type="text" v-model="config.value">
     </div>
 </template>
 
 <script>
-  import VueQArt from '../src/vue-qart.vue'
-  export default {
-    components: {
-      VueQArt
-    },
-    name: 'app',
-    data () {
-      return {
-        msg: 'the Vue 2.x directive for QArt.js',
-        config: {
-          value: 'https://www.baidu.com',
-          imagePath: './assets/logo.png',
-          filter: 'color',
-          size: 500
-        },
-        downloadButton: false
+import VueQArt from "../src/vue-qart.vue";
+export default {
+  components: {
+    VueQArt
+  },
+  name: "app",
+  data() {
+    return {
+      msg: "the Vue 2.x directive for QArt.js",
+      config: {
+        value: "https://www.baidu.com",
+        imagePath: "./assets/logo.png",
+        filter: "color",
+        size: 500
+      },
+      download: {
+        visible: true,
+        text: "下载",
+        style: {
+          fontSize: "16px"
+        }
       }
-    }
+    };
   }
+};
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
-    h1, h2 {
-        font-weight: normal;
-    }
+h1,
+h2 {
+  font-weight: normal;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-    a {
-        color: #42b983;
-    }
+a {
+  color: #42b983;
+}
 </style>
--- src/vue-qart.vue
+++ src/vue-qart.vue
@@ -1,45 +1,59 @@
 <template>
     <div>
         <div ref="qart"></div>
-        <button @click="convertToImage" v-if="downloadButton">download t2o image</button>
+        <button @click="convertToImage" v-if="download.visible" v-bind:style="download.style">{{download.text}}</button>
     </div>
 </template>
 
 
 <script type="application/ecmascript">
-import QArt from 'qartjs';
-  export default {
-    props: ['config', 'downloadButton'],
-    name: 'VueQart',
-  
-    data () {
-      return {
-        msg: '',
-      }
+import QArt from "qartjs";
+export default {
+  props: {
+    config: {
+      type: Object,
     },
-    watch: {
-      'config.value': function (val, oldVal) {
-        this.config.value = val;
-        this.renderQrcode(this.config);
-      console.log('new: %s, old: %s', val, oldVal)
-      },
-    },
-    mounted(){
-      this.renderQrcode(this.config)
-    },
-    
-    methods: {
-      renderQrcode(config){
-        this.qart =  new QArt(config);
-        this.qart.make(this.$refs.qart)
-      },
-      convertToImage(){
-        const myCanvas = this.$refs.qart.children[0];
-        const type = 'image/png';
-        let image = myCanvas.toDataURL(type).replace(type, "image/octet-stream");
-        window.location.href = image; // it will save locally
+    download: {
+      type: Object,
+      default: function(){
+        return {
+          visible: false,
+          text: '下载为图片',
+          style: {}
+        }
       }
     }
+  },
+  name: "VueQart",
+
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  watch: {
+    "config.value": function(val, oldVal) {
+      this.config.value = val;
+      this.renderQrcode(this.config);
+    }
+  },
+  mounted() {
+    this.renderQrcode(this.config);
+  },
+
+  methods: {
+    renderQrcode(config) {
+      this.qart = new QArt(config);
+      this.qart.make(this.$refs.qart);
+      // this.$refs.qart.appendChild(this.qart.make())
+    },
+    convertToImage() {
+      const myCanvas = this.$refs.qart.children[0];
+      const type = "image/png";
+      let image = myCanvas.toDataURL(type).replace(type, "image/octet-stream");
+      window.location.href = image; // it will save locally
+    }
   }
+};
 </script>
 
